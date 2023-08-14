b1943f5b7e8ec6c6181712c4ace4fafa7fbff584
inimomon <richiwirjaya@gmail.com>
2023-08-14 08:20:53 +0700

    nambah respon

---
+++ respon.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="respon_action.php" method="POST">
+        <div><input type="text" name="komentar" placeholder="Komentar"></div>
+
+        <button type="submit">Kirim Ulasan</button>
+    </form>
+</body>
+</html>
